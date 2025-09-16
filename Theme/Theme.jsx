90e54563b2f14acee33a7ac5b67b3f5e5a3fd21d
Flora-M04 <ece.22becd60@silicon.ac.in>
2025-09-16 17:16:18 +0000

    Online Sneaker Project

---
+++ Theme/Theme.jsx
@@ -0,0 +1,16 @@
+export const colors ={
+    light:{
+        primary:'white',
+        secondary:'white',
+        tertiary:'green',
+        txt:'#101014',
+        same:"#f0f0f0"
+    },
+    dark:{
+        primary:'#101014',
+        secondary:'#32363F',
+        tertiary:'#D4C4FC',
+        txt:'white',
+        same:"f0f0f0"
+    },
+}
