8aa29cede4cde2ad5241ffd6a69bf7d72d76e1f6
M.FLORA <ece.22becd60@silicon.ac.in>
2025-09-16 22:54:15 +0530

    Theme Changed

--- Theme/Theme.jsx
+++ Theme/Theme.jsx
@@ -2,14 +2,14 @@ export const colors ={
     light:{
         primary:'white',
         secondary:'white',
-        tertiary:'green',
+        tertiary:'purple',
         txt:'#101014',
         same:"#f0f0f0"
     },
     dark:{
         primary:'#101014',
         secondary:'#32363F',
-        tertiary:'#D4C4FC',
+        tertiary:'#790599ff',
         txt:'white',
         same:"f0f0f0"
     },
